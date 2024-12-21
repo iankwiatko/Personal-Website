7cfc3577874dcba84cde5764d4a6c50a07d9e599
Ian Kwiatkowski <iankwiatko@gmail.com>
2024-12-21 18:19:08 -0500

    added tailwind, now just need to remove unecessary css

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -3,22 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This will be my personal website.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1 class="text-5xl font-bold underline font-mono">
+          Hello world!
+        </h1>
       </header>
-    </div>
+    </div>  
   );
 }
 
